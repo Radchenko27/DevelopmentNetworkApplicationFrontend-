a6ac3fd573ede53e21b12bed29b96ba4cc2d0c1d
Дмитрий Радченко <dimaradchenko270304@yandex.ru>
2024-12-02 13:57:52 +0300

    Добавил еще картинки к деплою.

--- transport_insurance/src/mock/mockData.ts
+++ transport_insurance/src/mock/mockData.ts
@@ -1,42 +1,45 @@
 export interface Driver {
-    id: number;
-    name: string;
-    certificate_number: string;
-    license: string;
-    experience: number;
-    characteristics: string;
-    image_url: string | null;
-    status: string;
-  }
-  export const mockData: Driver[] = [
-    {
-      id: 1,
-      name: "Радченко Дмитрий Сергеевич",
-      certificate_number: "32 12 344234",
-      license: "B",
-      experience: 2,
-      image_url: null,
-      characteristics: "лучший,",
-      status: "active",
-    },
-    {
-      id: 2,
-      name: "Радченко Дмитрий Сергеевич",
-      certificate_number: "32 12 344234",
-      license: "B",
-      experience: 2,
-      image_url: null,
-      characteristics: "лучший,",
-      status: "active",
-    },
-    {
-      id: 3,
-      name: "Радченко Дмитрий Сергеевич",
-      certificate_number: "32 12 344234",
-      license: "B",
-      experience: 2,
-      image_url: null,
-      characteristics: "лучший,",
-      status: "active",
-    },
-  ];
+  id: number;
+  name: string;
+  certificate_number: string;
+  license: string;
+  experience: number;
+  characteristics: string;
+  image_url: string | null;
+  status: string;
+}
+export const mockData: Driver[] = [
+  {
+    id: 1,
+    name: "Радченко Дмитрий Сергеевич",
+    certificate_number: "32 12 344234",
+    license: "B",
+    experience: 2,
+    image_url:
+      "https://radchenko27.github.io/DevelopmentNetworkApplicationFrontend/images/1.png",
+    characteristics: "лучший,",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Радченко Дмитрий Сергеевич",
+    certificate_number: "32 12 344234",
+    license: "B",
+    experience: 2,
+    image_url:
+      "https://radchenko27.github.io/DevelopmentNetworkApplicationFrontend/images/1.png",
+    characteristics: "лучший,",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Радченко Дмитрий Сергеевич",
+    certificate_number: "32 12 344234",
+    license: "B",
+    experience: 2,
+    image_url:
+      "https://radchenko27.github.io/DevelopmentNetworkApplicationFrontend/images/1.png",
+    characteristics: "лучший,",
+    status: "active",
+  },
+];
